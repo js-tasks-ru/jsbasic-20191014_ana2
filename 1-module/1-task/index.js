960570eac9fd4a71bfb05dc5bc67ac1a989fdc1c
AnaBroderie <56192108+AnaBroderie@users.noreply.github.com>
2019-10-29 16:48:35 +0200

    pow function for 1 module 1 task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -5,5 +5,14 @@
  * @returns {number}
  */
 function pow(m, n) {
-
+  let result = 1;
+	for (i = 0; i < n; i++){
+		result = result * m;
+	}
+	return result;
 }
+
+let number   = prompt('Please insert number'),
+    exponent = prompt('Please insert exponent for number');
+
+alert( pow(number, exponent) );
