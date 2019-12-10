866df5fab76f7e01177ee83f28199e977b041ea5
AnaBroderie <56192108+AnaBroderie@users.noreply.github.com>
2019-12-10 19:43:27 +0200

    4 module 1 task ready

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,12 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  let friendsListHTML = document.createElement('ul');
+  friends.forEach(function (friend) {
+    let friendHTML = document.createElement('li');
+    friendHTML.innerText = friend.firstName + ' ' + friend.lastName;
+    friendsListHTML.appendChild(friendHTML);
+  });
+
+  return friendsListHTML;
 }
